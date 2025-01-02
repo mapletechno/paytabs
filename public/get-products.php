d9d4a3e43524254a0ae9ce41c3ca54c94cc3c303
mapletechno <ahmed@mapletechno.com>
2025-01-02 08:04:22 +0200

    Revert "Initial commit"
    This reverts commit 38e4fc3bce64ccd445735ee3ecf6eb018a331a1a.

--- public/get-products.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../autoload.php';
-
-
-//require_once __DIR__ . '/../src/Models/Product.php';
-use App\Models\Product;
-$productModel = new Product();
-echo json_encode($productModel->getAll());
-?>
--- views/orders.php
+++
@@ -1,7 +0,0 @@
-<?php foreach ($orders as $order): ?>
-    <div>
-        <h3>Order #<?= htmlspecialchars($order['id']) ?></h3>
-        <p>Status: <?= htmlspecialchars($order['status']) ?></p>
-        <p>Items: <?= htmlspecialchars($order['items']) ?></p>
-    </div>
-<?php endforeach; ?>
